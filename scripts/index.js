2be13a505c0751b134b21cd054561ca7f237f55e
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-03-11 16:05:32 +0800

    Make ctgrs carousel btns work

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+(() => {
+  const ptry_ctgrs_cont = document.querySelector(
+    "[data-slctr=pastry_ctgrs_cont]"
+  );
+  const ptry_btn = ptry_ctgrs_cont.querySelector("a");
+  const ptry_btn_dim = ptry_btn.getBoundingClientRect();
+
+  const ptry_ctgrs_left_btn = document.querySelector(
+    "[data-slctr=pastry_ctgrs_left_btn]"
+  );
+  const ptry_ctgrs_right_btn = document.querySelector(
+    "[data-slctr=pastry_ctgrs_right_btn]"
+  );
+
+  if (typeof ptry_ctgrs_cont.scrollBy === "function") {
+    ptry_ctgrs_right_btn.addEventListener("click", () => {
+      ptry_ctgrs_cont.scrollBy({
+        left: ptry_btn_dim.width + 20,
+        behavior: "smooth",
+      });
+    });
+
+    ptry_ctgrs_left_btn.addEventListener("click", () => {
+      ptry_ctgrs_cont.scrollBy({
+        left: -ptry_btn_dim.width - 20,
+        behavior: "smooth",
+      });
+    });
+  } else {
+    ptry_ctgrs_left_btn.remove();
+    ptry_ctgrs_right_btn.remove();
+  }
+})();
